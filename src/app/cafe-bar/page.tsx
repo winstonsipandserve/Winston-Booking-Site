457ee800c3ec7dc53d893e0794194fb170ef5014
rafael <arjayrafaelical@gmail.com>
2026-09-04 05:01:36 +0800

    Remove placeholder Hours/Location section from Cafe & Bar page

--- src/app/cafe-bar/page.tsx
+++ src/app/cafe-bar/page.tsx
@@ -1,14 +1,12 @@
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
 import CafeBarPageClient from '@/components/cafe-bar/CafeBarPageClient'
-import HoursLocation from '@/components/cafe-bar/HoursLocation'
 
 export default function CafeBarPage() {
   return (
     <>
       <Navbar />
       <CafeBarPageClient />
-      <HoursLocation />
       <Footer />
     </>
   )
